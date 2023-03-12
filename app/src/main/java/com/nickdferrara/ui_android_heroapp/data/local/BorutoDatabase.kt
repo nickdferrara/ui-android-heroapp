49eb84f82d95acaacd45404fc92ecc280aca2424
nickdferrara <nick.d.ferrara@gmail.com>
2023-03-12 15:06:29 -0400

    Make room database

---
+++ app/src/main/java/com/nickdferrara/ui_android_heroapp/data/local/BorutoDatabase.kt
@@ -0,0 +1,16 @@
+package com.nickdferrara.ui_android_heroapp.data.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.nickdferrara.ui_android_heroapp.data.local.dao.HeroDao
+import com.nickdferrara.ui_android_heroapp.data.local.dao.HeroRemoteKeyDao
+import com.nickdferrara.ui_android_heroapp.domain.models.Hero
+import com.nickdferrara.ui_android_heroapp.domain.models.HeroRemoteKey
+
+@Database(entities = [Hero::class, HeroRemoteKey::class], version = 1)
+@TypeConverters(DatabaseConverter::class)
+abstract class BorutoDatabase: RoomDatabase() {
+    abstract fun heroDao(): HeroDao
+    abstract fun heroRemoteKeyDao(): HeroRemoteKeyDao
+}
